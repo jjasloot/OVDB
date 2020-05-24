f12b759bcaa6e0193cce5d5dda747742ea3b88e3
Jaap Slootbeek <j.slootbeek@recognize.nl>
2020-05-25 00:06:58 +0200

    added test request

--- OV_DB/Controllers/AdminController.cs
+++ OV_DB/Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using OVDB_database.Database;
 
 namespace OV_DB.Controllers
@@ -13,10 +14,12 @@ namespace OV_DB.Controllers
     [ApiController]
     public class AdminController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
         private readonly OVDBDatabaseContext _dbContext;
 
-        public AdminController(OVDBDatabaseContext dbContext)
+        public AdminController(OVDBDatabaseContext dbContext, IConfiguration configuration)
         {
+            _configuration = configuration;
             _dbContext = dbContext;
         }
 
@@ -35,6 +38,18 @@ namespace OV_DB.Controllers
 
             await _dbContext.SaveChangesAsync();
             return Ok();
+        }
+
+        [HttpGet("useragent")]
+        public ActionResult GetUserAgent()
+        {
+            var adminClaim = (User.Claims.SingleOrDefault(c => c.Type == "admin").Value ?? "false");
+            if (string.Equals(adminClaim, "false", StringComparison.OrdinalIgnoreCase))
+            {
+                return Forbid();
+            }
+            var userAgent = _configuration.GetValue<string>("UserAgent", "OVDB");
+            return Ok(userAgent);
         }
     }
 }
