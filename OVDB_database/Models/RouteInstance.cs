6123ee93f93aef0b9bbd28e625689e2ce0f1b1b8
Jaap <j.slootbeek@recognize.nl>
2022-04-01 23:39:21 +0200

    Update RouteInstance.cs

--- OVDB_database/Models/RouteInstance.cs
+++ OVDB_database/Models/RouteInstance.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace OVDB_database.Models
 {
+    [Index(nameof(Date))]
     public class RouteInstance
     {
         [Key]
