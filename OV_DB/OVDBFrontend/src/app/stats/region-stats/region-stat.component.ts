45110e69b12ead3be69c183ed7e35951bf746b70
Jaap <jaapslootbeek@gmail.com>
2025-07-02 23:52:45 +0200

    Improve operator stats

--- OV_DB/OVDBFrontend/src/app/stats/region-stats/region-stat.component.ts
+++ OV_DB/OVDBFrontend/src/app/stats/region-stats/region-stat.component.ts
@@ -1,24 +1,23 @@
-import { Component, inject, Input, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { RegionStat } from '../../models/region.model';
 import { ApiService } from '../../services/api.service';
-import { NgFor } from '@angular/common';
 import { TranslationService } from '../../services/translation.service';
 import { RegionStatsDisplayComponent } from "./region-stats-display/region-stats-display.component";
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-region-stat',
   templateUrl: './region-stat.component.html',
   styleUrls: ['./region-stat.component.scss'],
   standalone: true,
-  imports: [MatListModule, MatIconModule, MatProgressBarModule, RegionStatsDisplayComponent]
+  imports: [MatListModule, MatIconModule, MatProgressBarModule, RegionStatsDisplayComponent, MatProgressSpinnerModule]
 })
 export class RegionStatComponent implements OnInit {
   regionStats = signal([] as RegionStat[]);
-
+  loading = signal(true);
   private readonly apiService = inject(ApiService);
   private readonly translationService = inject(TranslationService);
 
@@ -27,6 +26,7 @@ export class RegionStatComponent implements OnInit {
   ngOnInit(): void {
     this.apiService.getRegionStats().subscribe(stats => {
       this.regionStats.set(stats);
+      this.loading.set(false);
     });
   }
 
--- OV_DB/OVDBFrontend/src/app/used-operators/used-operators.component.ts
+++ OV_DB/OVDBFrontend/src/app/used-operators/used-operators.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit, inject, signal } from "@angular/core";
+import { Component, OnInit, computed, inject, signal } from "@angular/core";
 import { ApiService } from "src/app/services/api.service";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
 import { MatDividerModule } from "@angular/material/divider";
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
 import { AsyncPipe, NgClass } from "@angular/common";
 import { OperatorService } from "../services/operator.service";
 import { RegionOperator, RegionOperators } from "../models/region-operators.model";
 import { TranslationService } from "../services/translation.service";
+import { toSignal } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: "app-used-operators",
@@ -19,15 +23,33 @@ import { TranslationService } from "../services/translation.service";
     MatIconModule,
     MatListModule,
     MatDividerModule,
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+    FormsModule,
     AsyncPipe,
     NgClass
-],
+  ],
 })
 export class UsedOperatorsComponent implements OnInit {
   apiService = inject(ApiService);
   operatorService = inject(OperatorService)
   translationService = inject(TranslationService);
   regions = signal<RegionOperators[]>([]);
+  compact = false;
+  currentLanguage = toSignal(this.translationService.languageChanged);
+
+  sortedRegion = computed(() => {
+    this.currentLanguage(); //so we update when the language changes
+    return this.regions().sort((a, b) => {
+      if (this.name(a) < this.name(b)) {
+        return -1;
+      }
+      if (this.name(a) > this.name(b)) {
+        return 1;
+      }
+      return 0;
+    })
+  });
 
   ngOnInit(): void {
     this.apiService.getOperatorsGroupedByRegion().subscribe((data) => {
@@ -41,5 +63,9 @@ export class UsedOperatorsComponent implements OnInit {
 
   name(region: RegionOperators) {
     return this.translationService.getNameForItem(region);
+  }
+
+  toggleCompact() {
+    this.compact = !this.compact;
   }
 }
