9c188891de21915bf5bd606a2292ab5746ce4322
Jaap <jaapslootbeek@gmail.com>
2025-07-23 18:52:27 +0200

    Update angular to v20

--- OV_DB/OVDBFrontend/src/app/app.component.ts
+++ OV_DB/OVDBFrontend/src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserPreferenceService } from './services/user-preference.service';
-
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    imports: [RouterOutlet]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  imports: [RouterOutlet]
 })
 export class AppComponent implements OnInit {
 
   constructor(private userPreferenceService: UserPreferenceService) { }
-
   ngOnInit() {
     // Apply user language preference if logged in
     setTimeout(() => {
